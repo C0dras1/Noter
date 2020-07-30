0d9871cca60b2dfa552b41216c4c6211e0ced816
Jesse <jessesuderman@gmail.com>
2020-07-29 19:13:35 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+// Your web app's Firebase configuration
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyBgVUiFFIqSn1Op0Om9CW9SrzBIqVkT-zI",
+  authDomain: "noter-c12a8.firebaseapp.com",
+  databaseURL: "https://noter-c12a8.firebaseio.com",
+  projectId: "noter-c12a8",
+  storageBucket: "noter-c12a8.appspot.com",
+  messagingSenderId: "1012984912724",
+  appId: "1:1012984912724:web:80fe9395687e9bc88306c1",
+  measurementId: "G-6CTJTD6TVR"
+});
+firebase.analytics();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
